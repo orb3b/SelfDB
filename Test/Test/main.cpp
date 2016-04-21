0b9171d4ebb282260b96ece4bd7fd0f2f27d4140
orb3bb <orb3bb@gmail.com>
2016-04-21 11:39:12 +0300

    Demo for sort and group by

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -9,23 +9,44 @@ using namespace std;
 
 const char* FILE_NAME = "data50m.bin";
 const int SIZE = 50000000;
-const int BUFFER_SIZE = 1000000;
+const int BUFFER_SIZE = 10000000;
+
+char* categories[] = {
+    "Sugar",
+    "Salt",
+    "Milk",
+    "Meat",
+    "Chicken",
+    "Vegetables",
+    "Beer",
+    "Oils",
+    "Fish",
+    "Juice",
+    "Sigarrets",
+    "Bread"
+};
+int categoriesSize = 12;
 
 struct Row {
     int id;
-    char category[80];
+    char category[50];
 };
 
 class Database {
-    Row **data;
+    Row *data;
     int blocksCount;
 
 public:
     Row* at(int index) {
-        auto block = index / BUFFER_SIZE;
-        auto offset = index % BUFFER_SIZE;
+        //auto block = index / BUFFER_SIZE;
+        //auto offset = index % BUFFER_SIZE;
 
-        return &(data[block][offset]);
+        return &(data[index]);
+    }
+
+    void sort()
+    {
+        qsort(data, SIZE, sizeof(Row), Database::compare);
     }
 
     bool load() {
@@ -42,15 +63,22 @@ public:
             blocksCount++;
         }
 
-        data = new Row*[blocksCount];
+        data = new Row[SIZE];
         for (auto i = 0; i < blocksCount; i++) {
-            data[i] = new Row[BUFFER_SIZE];
-            fread(data[i], sizeof(Row), BUFFER_SIZE, fp);
+            fread(data + i * BUFFER_SIZE, sizeof(Row), BUFFER_SIZE, fp);
         }
 
         fclose(fp);
 
         return true;
+    }
+private:
+    static int compare(const void* p1, const void* p2)
+    {
+        auto row1 = (Row*)p1;
+        auto row2 = (Row*)p2;
+
+        return row1->id - row2->id;
     }
 };
 
@@ -62,11 +90,6 @@ bool generate(int size)
         return false;
     }
 
-    char* categories[] = {
-        "Бакалея, сахар, соль",
-        "Консервы"
-    };
-
     auto blocksCount = SIZE / BUFFER_SIZE;
     if (SIZE % BUFFER_SIZE != 0) {
         blocksCount++;
@@ -76,7 +99,7 @@ bool generate(int size)
     for (auto i = 0; i < blocksCount; i++) {
         for (auto i = 0; i < BUFFER_SIZE; i++) {
             array[i].id = rand();
-            strcpy(array[i].category, categories[rand() % 2]);
+            strcpy(array[i].category, categories[rand() % categoriesSize]);
         }
         fwrite(array, sizeof(Row), BUFFER_SIZE, fp);
     }
@@ -86,24 +109,6 @@ bool generate(int size)
 
     return true;
 }
-
-//bool load(Row **container)
-//{
-//    auto fp = fopen(FILE_NAME, "rb");
-//    if (!fp) {
-//        printf("Cannot open %s for reading!", FILE_NAME);
-//        return false;
-//    }
-
-//    fseek(fp, 0, SEEK_SET);
-
-//    *container = new Row[SIZE];
-//    fread(*container, sizeof(Row), SIZE, fp);
-
-//    fclose(fp);
-
-//    return true;
-//}
 
 void writeLine(char *line)
 {
@@ -149,23 +154,44 @@ int main(int argc, char *argv[])
     long long elapsed = 0;
     long long t_start = QDateTime::currentMSecsSinceEpoch();
 
-//    cout << "Searching value..." << endl;
-//    auto count1 = 0;
-//    auto count2 = 0;
-//    for (auto i = 0; i < SIZE; i++) {
-//        if (strcmp(db.at(i)->category, "Консервы") == 0) {
-//            count1++;
-//        } else {
-//            count2++;
-//        }
+    // Group by
+    cout << "Group by..." << endl;
+
+    auto counts = new int[categoriesSize];
+    memset(counts, 0, categoriesSize * sizeof(int));
+
+    for (auto i = 0; i < SIZE; i++) {
+        auto category = db.at(i)->category;
+        for (auto j = 0; j < categoriesSize; j++) {
+            if (strcmp(category, categories[j]) == 0) {
+                counts[j]++;
+                break;
+            }
+        }
+    }
+
+    for (auto i = 0; i < categoriesSize; i++) {
+        printf("%s: %i\n", categories[i], counts[i]);
+    }
+
+//    // Sort
+//    for (auto i = 1000000; i < 1000100; i++) {
+//        auto row = db.at(i);
+//        printf("%i %s\n", row->id, row->category);
+//    }
+
+//    cout << "Sorting..." << endl;
+
+//    db.sort();
+
+//    for (auto i = 1000000; i < 1000100; i++) {
+//        auto row = db.at(i);
+//        printf("%i %s\n", row->id, row->category);
 //    }
 
     // Bench end
     long long t_end = QDateTime::currentMSecsSinceEpoch();
     elapsed = t_end - t_start;
-
-    printf("Count Консервы: %i\n", count1);
-    printf("Count Бакалея, сахар, соль: %i\n", count2);
 
     printf("Elapsed time: %i\n", elapsed);
 
